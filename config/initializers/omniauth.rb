91d43d0bce6f441ad62f7c50b5717ffe359a6e4b
Gor <7495500@gmail.com>
2025-12-09 20:59:41 +0300

    Fix test essue

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,17 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+  client_id, client_secret =
+    if Rails.env.test?
+      ['test_client_id', 'test_client_secret']
+    else
+      [
+        ENV.fetch('GITHUB_CLIENT_ID'),
+        ENV.fetch('GITHUB_CLIENT_SECRET')
+      ]
+    end
+
   provider :github,
-           ENV.fetch('GITHUB_CLIENT_ID'),
-           ENV.fetch('GITHUB_CLIENT_SECRET'),
+           client_id,
+           client_secret,
            scope: 'read:user,user:email,repo'
 end
 
