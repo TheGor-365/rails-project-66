947af05a75b5a29c7132af2d60bbf63404c622cd
TheGor-365 <7495500@gmail.com>
2025-12-15 22:55:17 +0300

    Fix dotenv loading for prod + make OmniAuth optional without env vars

--- config/application.rb
+++ config/application.rb
@@ -4,15 +4,20 @@ require "rails/all"
 
 Bundler.require(*Rails.groups)
 
-begin
-  require "dotenv/rails-now"
-rescue LoadError
+if %w[development test].include?(ENV.fetch("RAILS_ENV", "development"))
+  begin
+    require "dotenv/load"
+  rescue LoadError
+  end
 end
 
 module RailsProject66
   class Application < Rails::Application
     config.load_defaults 7.2
     config.autoload_lib(ignore: %w[assets tasks])
-    routes.default_url_options = { host: ENV.fetch("BASE_URL", "http://localhost:3000") }
+
+    routes.default_url_options = {
+      host: ENV.fetch("BASE_URL", "http://localhost:3000")
+    }
   end
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,23 +1,23 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  client_id, client_secret =
-    if Rails.env.test?
-      ["test_client_id", "test_client_secret"]
-    else
-      [
-        ENV["GITHUB_CLIENT_ID"],
-        ENV["GITHUB_CLIENT_SECRET"]
-      ]
-    end
+require "omniauth-github"
+
+scope = "read:user,user:email,repo"
+
+if Rails.env.test?
+  Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :github, "test_client_id", "test_client_secret", scope: scope
+  end
+else
+  client_id = ENV["GITHUB_CLIENT_ID"]
+  client_secret = ENV["GITHUB_CLIENT_SECRET"]
 
   if client_id.present? && client_secret.present?
-    provider :github,
-             client_id,
-             client_secret,
-             scope: "read:user,user:email,repo"
+    Rails.application.config.middleware.use OmniAuth::Builder do
+      provider :github, client_id, client_secret, scope: scope
+    end
   else
     Rails.logger.warn("[OmniAuth] GitHub OAuth env vars are missing; provider is not configured")
   end
 end
 
 OmniAuth.config.allowed_request_methods = [:post]
-OmniAuth.config.request_validation_phase = nil
+OmniAuth.config.request_validation_phase = nil if Rails.env.test?
