663bfb12e50a7056cd1442a6cd0c877482d8a139
TheGor-365 <7495500@gmail.com>
2025-12-20 22:46:56 +0300

    Modify puma.rb 2

--- config/puma.rb
+++ config/puma.rb
@@ -1,8 +1,16 @@
-threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
-threads threads_count, threads_count
+# frozen_string_literal: true
 
-bind "tcp://0.0.0.0:#{ENV.fetch("PORT", 3000)}"
+max_threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
+min_threads_count = ENV.fetch("RAILS_MIN_THREADS", max_threads_count)
+threads min_threads_count, max_threads_count
+
+environment ENV.fetch("RAILS_ENV", "production")
+
+port = ENV.fetch("PORT", 3000)
+
+bind "tcp://0.0.0.0:#{port}"
+
+pidfile ENV.fetch("PIDFILE", "tmp/pids/server.pid")
+workers ENV.fetch("WEB_CONCURRENCY", 1)
 
 plugin :tmp_restart
-
-pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
