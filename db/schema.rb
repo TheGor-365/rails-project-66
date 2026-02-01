cee1e5d960da3c56a41143b20cbfcfe1f74e93ed
TheGor-365 <7495500@gmail.com>
2026-02-01 17:27:56 +0300

    Add sqlite

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_12_07_230803) do
+ActiveRecord::Schema[7.2].define(version: 2026_01_28_203127) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_12_07_230803) do
     t.string "ssh_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id", "github_id"], name: "index_repositories_on_user_id_and_github_id", unique: true
     t.index ["user_id"], name: "index_repositories_on_user_id"
   end
 
@@ -31,7 +32,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_12_07_230803) do
     t.bigint "repository_id", null: false
     t.string "status", default: "pending", null: false
     t.string "commit_id"
-    t.boolean "passed"
+    t.boolean "passed", default: false, null: false
     t.integer "violations_count"
     t.text "output"
     t.datetime "created_at", null: false
