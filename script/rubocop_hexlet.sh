782081f4f3de9c330693de7e80b74bc2a9a85e52
TheGor-365 <7495500@gmail.com>
2026-02-26 01:47:14 +0300

    CI: add hexlet-like rubocop config and runner script

---
+++ script/rubocop_hexlet.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set +e
+
+TMP_DIR=".tmp/hexlet_ci_layout"
+rm -rf "$TMP_DIR"
+mkdir -p "$TMP_DIR/code"
+
+rsync -a \
+  --exclude '.git' \
+  --exclude '.tmp' \
+  --exclude 'node_modules' \
+  ./ "$TMP_DIR/code/"
+
+cp .rubocop.hexlet.yml "$TMP_DIR/.rubocop.yml"
+
+cd "$TMP_DIR" || exit 1
+
+export BUNDLE_GEMFILE="$PWD/code/Gemfile"
+
+bundle exec rubocop code --config ./.rubocop.yml
+echo "rubocop_rc=$?"
