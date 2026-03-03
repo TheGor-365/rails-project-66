05d3c2c08b1c2583be990ba57aab66173258d6fe
TheGor-365 <7495500@gmail.com>
2026-03-03 22:25:43 +0300

    Test: remove broken set_fixture_class mapping

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -15,13 +15,6 @@ module ActiveSupport
   class TestCase
     parallelize(workers: :number_of_processors)
 
-set_fixture_class(
-  {
-    repository_checks: Check,
-    'repository/checks' => Check
-  }
-)
-
     fixtures :all
   end
 end
