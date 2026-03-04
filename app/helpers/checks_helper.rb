78c8bbcbd1c4a2bc170e6bccc5bc5ad53f729f8c
TheGor-365 <7495500@gmail.com>
2026-03-05 01:49:14 +0300

    Refactor: move check commit formatting from model to helpers

---
+++ app/helpers/checks_helper.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module ChecksHelper
+  SHORT_SHA_LENGTH = 7
+
+  def check_short_commit_id(check)
+    commit_id = check&.commit_id
+    return if commit_id.blank?
+
+    commit_id.to_s[0, SHORT_SHA_LENGTH]
+  end
+
+  def check_github_commit_url(check)
+    repo_full_name = check&.repository&.full_name
+    short = check_short_commit_id(check)
+    return if repo_full_name.blank? || short.blank?
+
+    "https://github.com/#{repo_full_name}/commit/#{short}"
+  end
+end
--- app/models/check.rb
+++ app/models/check.rb
@@ -27,23 +27,9 @@ class Check < ApplicationRecord
     end
   end
 
-  SHORT_SHA_LENGTH = 7
-
   def human_status
     return I18n.t("checks.aasm_state.#{aasm_state}", default: aasm_state.to_s) unless finished?
 
     I18n.t("checks.result_status.#{status}", default: status.to_s)
-  end
-
-  def short_commit_id
-    return if commit_id.blank?
-
-    commit_id[0, SHORT_SHA_LENGTH]
-  end
-
-  def github_commit_url
-    return if repository.blank? || repository.full_name.blank? || short_commit_id.blank?
-
-    "https://github.com/#{repository.full_name}/commit/#{short_commit_id}"
   end
 end
