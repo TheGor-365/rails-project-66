16ead276f42ea64e3c4c73d8281a4f37f13af172
TheGor-365 <7495500@gmail.com>
2026-03-05 02:02:17 +0300

    Refactor: move ApplicationContainer and stubs to lib (no requires)

--- config/initializers/00_test_support.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-return unless Rails.env.test?
-
-require Rails.root.join('test/support/code_checker_stub')
-require Rails.root.join('test/support/github_client_stub')
